56a79f225c9c61209e1926c0fe80dbb5a147b3fb
North Dunne <northdunne@gmail.com>
2025-05-09 18:09:55 +0000

    Have this version automatically update to fit the current version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import {useState, useEffect} from 'react';
+import {useState, useEffect, Suspense} from 'react';
 import {Input} from '@/components/ui/input';
 import {Label} from '@/components/ui/label';
 import {
@@ -324,3 +324,4 @@ export default function Home() {
     </div>
   );
 }
+
